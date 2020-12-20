53e8d3a7b26c5baa577f7150f2a3f779bc2f1f16
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2020-12-20 20:09:41 +0000

    Updated base test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,20 +2,36 @@
 
 declare(strict_types=1);
 
-namespace Chinstrap\Plugins\DynamicSitemap\Tests;
+namespace Chinstrap\Tests;
 
 use Chinstrap\Core\Kernel\AppFactory;
 use Chinstrap\Core\Kernel\ContainerFactory;
+use Laminas\Stratigility\MiddlewarePipe;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Psr\Container\ContainerInterface;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
     use MockeryPHPUnitIntegration;
 
+    /**
+     * Container instance
+     *
+     * @var ContainerInterface|null
+     */
+    protected $container;
+
+    /**
+     * App instance
+     *
+     * @var MiddlewarePipe|null
+     */
+    protected $app;
+
     public function setUp(): void
     {
         if (!defined('ROOT_DIR')) {
-            define('ROOT_DIR', __DIR__ . '/../');
+            define('ROOT_DIR', __DIR__ . '/');
         }
         if (!defined('CONTENT_PATH')) {
             define('CONTENT_PATH', 'content/');
